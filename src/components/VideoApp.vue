a1fbe56f84021ac859fc8d71e31eeba34c7cb336
pmarino2013 <pmarino2013@gmail.com>
2023-07-30 22:38:56 -0300

    Agrego videos y desarrollo elección de videos random

--- src/components/VideoApp.vue
+++ src/components/VideoApp.vue
@@ -1,9 +1,33 @@
 <template>
   <video muted loop autoplay>
-    <source src="../assets/videos/dev1.mp4" type="video/mp4" />
+    <source :src="videito" type="video/mp4" />
   </video>
 </template>
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+const videito = ref(null);
+onMounted(() => {
+  videoRandom();
+});
+function videoRandom() {
+  let opcion = Math.ceil(Math.random() * 4);
+
+  switch (opcion) {
+    case 1:
+      videito.value = "/videos/dev1.mp4";
+      break;
+    case 2:
+      videito.value = "/videos/dev2.mp4";
+      break;
+    case 3:
+      videito.value = "/videos/dev3.mp4";
+      break;
+    case 4:
+      videito.value = "/videos/dev4.mp4";
+      break;
+  }
+}
+</script>
 <style scope>
 video {
   position: absolute;
