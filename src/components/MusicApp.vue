1f4fcd1a5a786aa12382239d6ea6fad26da44499
pmarino2013 <pmarino2013@gmail.com>
2023-08-03 11:39:26 -0300

    Agrego la funcion de música aleatoria

--- src/components/MusicApp.vue
+++ src/components/MusicApp.vue
@@ -1,13 +1,39 @@
 <template>
-  <audio ref="sound" class="margen" loop :src="musica" type="audio/mp3" />
+  <audio
+    ref="sound"
+    class="margen"
+    loop
+    :src="musicSelected"
+    type="audio/mp3"
+  />
 </template>
 <script setup>
 const props = defineProps({
   play: Boolean,
 });
-import { ref, onMounted } from "vue";
-import musica from "../assets/music01.mp3";
+import { ref, onMounted, computed } from "vue";
+import musica1 from "/music/music01.mp3";
+import musica2 from "/music/music02.mp3";
+
 const sound = ref(null);
+const musica = ref(musica1);
+
+const musicArray = ref([musica1, musica2]);
+
+const musicSelected = computed(() => {
+  // let opcion = Math.ceil(Math.random() * 2);
+  let opcion = Math.round(Math.random() * 1);
+
+  return (musica.value = musicArray.value[opcion]);
+});
+
+//   switch (opcion) {
+//     case 1:
+//       return (musica.value = musica1);
+//     case 2:
+//       return (musica.value = musica2);
+//   }
+// });
 function playMusic() {
   sound.value.play();
 }
