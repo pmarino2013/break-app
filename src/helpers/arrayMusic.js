7fc8b199d1831dfe880aafe86f1a6b0d2a1959e6
pmarino2013 <pmarino2013@gmail.com>
2023-08-09 13:52:54 -0300

    Agrego nuevas canciones y datos de derechos

--- src/helpers/arrayMusic.js
+++ src/helpers/arrayMusic.js
@@ -3,5 +3,23 @@ import musica2 from "/music/music02.ogg";
 import musica3 from "/music/music03.ogg";
 import musica4 from "/music/music04.ogg";
 import musica5 from "/music/music05.ogg";
+import musica6 from "/music/music06.ogg";
+import musica7 from "/music/music07.ogg";
+import musica8 from "/music/music08.ogg";
 
-export const musicArray = [musica1, musica2, musica3, musica4, musica5];
+export const musicArray = [
+  { track: musica1, derechos: "" },
+  { track: musica2, derechos: "" },
+  { track: musica3, derechos: "" },
+  { track: musica4, derechos: "" },
+  { track: musica5, derechos: "" },
+  { track: musica6, derechos: '"Life is Pain (with Baasik)", Black Gryph0n' },
+  {
+    track: musica7,
+    derechos: '"What Marker" Music provided by https://Slip.stream',
+  },
+  {
+    track: musica8,
+    derechos: '"Doing That Work" Música proporcionada por https://slip.stream',
+  },
+];
