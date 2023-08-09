31ec84d660cffa8d4d910548611b4a79d095c4a3
pmarino2013 <pmarino2013@gmail.com>
2023-08-09 20:38:39 -0300

    Reemplazo los archivos fisicos de musica y videos por enlaces de cloudinary

--- src/helpers/arrayMusic.js
+++ src/helpers/arrayMusic.js
@@ -1,36 +1,64 @@
-import musica1 from "/music/music01.ogg";
-import musica2 from "/music/music02.ogg";
-import musica3 from "/music/music03.ogg";
-import musica4 from "/music/music04.ogg";
-import musica5 from "/music/music05.ogg";
-import musica6 from "/music/music06.ogg";
-import musica7 from "/music/music07.ogg";
-import musica8 from "/music/music08.ogg";
-import musica9 from "/music/music09.ogg";
-import musica10 from "/music/music10.ogg";
+// import musica1 from "/music/music01.ogg";
+// import musica2 from "/music/music02.ogg";
+// import musica3 from "/music/music03.ogg";
+// import musica4 from "/music/music04.ogg";
+// import musica5 from "/music/music05.ogg";
+// import musica6 from "/music/music06.ogg";
+// import musica7 from "/music/music07.ogg";
+// import musica8 from "/music/music08.ogg";
+// import musica9 from "/music/music09.ogg";
+// import musica10 from "/music/music10.ogg";
 
 export const musicArray = [
-  { track: musica1, derechos: "" },
-  { track: musica2, derechos: "" },
-  { track: musica3, derechos: "" },
-  { track: musica4, derechos: "" },
-  { track: musica5, derechos: "" },
-  { track: musica6, derechos: '"Life is Pain (with Baasik)", Black Gryph0n' },
   {
-    track: musica7,
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623973/music01_e8sjsd.ogg",
+    derechos: "",
+  },
+  {
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623975/music02_cohe4q.ogg",
+    derechos: "",
+  },
+  {
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623976/music03_yzd90m.ogg",
+    derechos: "",
+  },
+  {
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623976/music04_teyrtj.ogg",
+    derechos: "",
+  },
+  {
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623975/music05_gtgmds.ogg",
+    derechos: "",
+  },
+  {
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623973/music06_ksglh2.ogg",
+    derechos: '"Life is Pain (with Baasik)", Black Gryph0n',
+  },
+  {
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623978/music07_mwjb9n.ogg",
     derechos: '"What Marker" Music provided by https://Slip.stream',
   },
   {
-    track: musica8,
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623978/music08_gekbgr.ogg",
     derechos: '"Doing That Work" Música proporcionada por https://slip.stream',
   },
   {
-    track: musica9,
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623983/music09_rxjupu.ogg",
     derechos:
       '"TIGERCLAW", Mifune Música proporcionada por https://slip.stream',
   },
   {
-    track: musica10,
+    track:
+      "https://res.cloudinary.com/dg3bw86m3/video/upload/v1691623981/music10_xavrcu.ogg",
     derechos: '"This Is Facts" Música proporcionada por https://slip.stream',
   },
 ];
